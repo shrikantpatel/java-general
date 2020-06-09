48eb7bd43e1cb431397f57a2da4d8baa9c299c58
shrikant patel <>
2020-06-08 21:34:15 -0500

    few additional sample pratice code.

---
+++ src/main/java/com/shri/demo/leet/Print_In_Order.java
@@ -0,0 +1,58 @@
+package com.shri.demo.leet;
+
+import java.util.concurrent.Semaphore;
+
+/*
+1114. Print in Order
+
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+Example 1:
+
+Input: [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+Example 2:
+
+Input: [1,3,2]
+Output: "firstsecondthird"
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+ */
+public class Print_In_Order {
+
+    private Semaphore semaphore1 = new Semaphore(0);
+    private Semaphore semaphore2 = new Semaphore(0);
+
+    public Print_In_Order() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        semaphore1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        semaphore1.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        semaphore2.release();
+
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        semaphore2.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
