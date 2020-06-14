418097b5da0393f766f2b5239d0b2e10a9882adb
shrikant patel <>
2020-06-13 20:02:56 -0500

    Add code for string to integer.

--- src/main/java/com/shri/demo/leet/String_to_Integer.java
+++ src/main/java/com/shri/demo/leet/String_to_Integer.java
@@ -1,5 +1,9 @@
 package com.shri.demo.leet;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /*
 8. String to Integer (atoi)
 
@@ -46,10 +50,138 @@ Explanation: The number "-91283472332" is out of the range of a 32-bit signed in
              Thefore INT_MIN (−231) is returned.
  */
 public class String_to_Integer {
+
+
     public int myAtoi(String str) {
 
-        str.chars().filter
+        boolean numberStart = false;
+        str = str.trim();
+        StringBuilder strbuff = new StringBuilder();
 
-        return 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            // integral part has not started
+            if (!numberStart) {
+
+                // if initial space ignore it
+                if (c == ' ') continue;
+
+                // hit first integer, -, +
+                if (c == '+' || c == '-' || (c >= 47 && c <= 57)) {
+                    numberStart = true;
+                    strbuff.append(c);
+                }
+                // if the initial character are neither -, +, integer, it not valid format
+                else {
+                    return 0;
+                }
+            }
+            // if integral part has started only consider integer from that point onwards
+            // if you get anything else discontinue parsing
+            else {
+                if (c >= 47 && c <= 57) {
+                    strbuff.append(c);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        int answer = 0;
+        if (numberStart) {
+
+            String finalString = strbuff.toString();
+
+            switch (finalString) {
+                case "-":
+                case "+":
+                    break;
+                default:
+                    try {
+                        answer = Integer.parseInt(new String(strbuff));
+                    } catch (NumberFormatException nme) {
+                        if (finalString.contains("-")) return Integer.MIN_VALUE;
+                        else return Integer.MAX_VALUE;
+                    }
+            }
+        }
+        return answer;
     }
+
+    @Test
+    public void test1() {
+        assertEquals(90, (new String_to_Integer()).myAtoi("90"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(-42, (new String_to_Integer()).myAtoi("   -42"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("words and 987"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(4193, (new String_to_Integer()).myAtoi("4193 with words"));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(Integer.MIN_VALUE, (new String_to_Integer()).myAtoi("-91283472332"));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(0, (new String_to_Integer()).myAtoi(""));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("+"));
+    }
+
+    @Test
+    public void test8() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("-"));
+    }
+
+    @Test
+    public void test9() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("+-2"));
+    }
+
+    @Test
+    public void test10() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("-+2"));
+    }
+
+    @Test
+    public void test11() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("  +0 123"));
+    }
+
+    @Test
+    public void test12() {
+        assertEquals(2147483647, (new String_to_Integer()).myAtoi("2147483648"));
+    }
+
+    @Test
+    public void test13() {
+        assertEquals(2147483647, (new String_to_Integer()).myAtoi("2147483800"));
+    }
+
+    @Test
+    public void test14() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("-   234"));
+    }
+
+    @Test
+    public void test15() {
+        assertEquals(0, (new String_to_Integer()).myAtoi("         "));
+    }
+
 }
