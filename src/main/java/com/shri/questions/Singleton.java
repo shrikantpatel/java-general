38d8bbc4a308a077f71c4d3ee8ccca9c0d0cffdd
shpatel <shrikant_patelin@yahoo.com>
2023-08-07 16:11:59 -0500

    Pratice session for a singleton.

---
+++ src/main/java/com/shri/questions/Singleton.java
@@ -0,0 +1,23 @@
+package com.shri.questions;
+
+public class Singleton {
+
+    private static volatile Singleton instance = null;
+
+    private Singleton() {
+        if (instance != null) {
+            throw new RuntimeException("Dont get constructor to get instance for singleton !!");
+        }
+    }
+
+    public Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
