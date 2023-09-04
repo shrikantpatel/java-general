6fc87a35b416be17d911fe4aeedccd7bf1675c13
shpatel <shrikant_patelin@yahoo.com>
2023-09-04 10:30:58 -0500

    moving longest non repeating character set from array.

--- src/main/java/com/shri/demo/leet/Longest_NonRepeating_Sub_String_Length.java
+++ src/main/java/com/shri/demo/leet/Longest_NonRepeating_Sub_String_Length.java
@@ -6,38 +6,109 @@ import org.junit.Test;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * <p>
+ * 3. Longest Substring Without Repeating Characters
+ * Medium
+ * 36.8K
+ * 1.7K
+ * Companies
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * <p>
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * <p>
+ * Example 2:
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * <p>
+ * Example 3:
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= s.length <= 5 * 104
+ * s consists of English letters, digits, symbols and spaces.
+ */
 public class Longest_NonRepeating_Sub_String_Length {
-    public static long solution(String str) {
+    public static int solution(String str) {
 
-        int length = str.length();
-        long longestSubString = 0;
-        char c;
+        /**
+         * Use the sliding window
+         */
+        char[] input = str.toCharArray();
+        Set<Character> uniqueChars = new HashSet<>();
 
-        for(int i = 0; i < length; i++)
-        {
-            Set<Character> visited = new HashSet<Character>();
-            int currentLongestSubString = 0;
+        // variable to store the start index and end index
+        int maxLength = 0;
+        int startIndex = 0;
+        int endIndex = 0;
 
-            for(int j = i; j < length; j++)
-            {
 
-                c = str.charAt(j);
-                if (visited.contains(c))
-                    break;
-                else
-                {
-                    visited.add(c);
-                    currentLongestSubString++;
-                }
+        for (char c : input) {
+
+            if (!uniqueChars.contains(c)) {
+                uniqueChars.add(c);
             }
-            longestSubString = Math.max(longestSubString, currentLongestSubString);
+            // when current char exists in set, it duplicate and we need to slide the window
+            else {
+
+                // check the current end and if its greater than maxlength, we have got new maxlength.
+                int tempLength = endIndex - startIndex;
+                maxLength = tempLength > maxLength ? tempLength : maxLength;
+
+                // slide the window, start from start index, continue to move right till hit the repeating character
+                // as you moving through remove all element that you encounter on the way from set,
+                // since they need to discarded from subsequent unique char check
+                int i = startIndex;
+                while (str.charAt(i) != c) {
+                    uniqueChars.remove(str.charAt(i));
+                    i++;
+                }
+                // last loop stop at exactly when we encounter repeating character
+                // so we need increment start index by 1 to get to next character index.
+                startIndex = i + 1;
+            }
+            endIndex++;
         }
 
-        return longestSubString;
+        int tempLength = endIndex - startIndex;
+        maxLength = tempLength > maxLength ? tempLength : maxLength;
+
+        return maxLength;
+
     }
 
     @Test
     public void test1() {
         Assert.assertEquals(4, Longest_NonRepeating_Sub_String_Length.solution("nndNfdfdf"));
     }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(3, Longest_NonRepeating_Sub_String_Length.solution("dvdf"));
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(3, Longest_NonRepeating_Sub_String_Length.solution("abcabcbb"));
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertEquals(1, Longest_NonRepeating_Sub_String_Length.solution("bbbbb"));
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertEquals(3, Longest_NonRepeating_Sub_String_Length.solution("pwwkew"));
+    }
+
 }
