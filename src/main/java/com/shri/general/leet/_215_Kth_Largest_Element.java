05dd0026d5df6530a50490b7772eab9cf74f578b
shrikantpatel <shrikant_patelin@yahoo.com>
2023-11-14 09:06:46 -0600

    Solved the leet code #215 using heapify approach using Quick selection algo

--- src/main/java/com/shri/general/leet/_215_Kth_Largest_Element.java
+++ src/main/java/com/shri/general/leet/_215_Kth_Largest_Element.java
@@ -32,7 +32,7 @@ public class _215_Kth_Largest_Element {
 
     /**
      * Approach 1: Min-Heap
-     *
+     * <p>
      * The main idea of this solution is to use a min-heap with a maximum size of k.
      * By doing this, we ensure that the smallest of the k largest elements is always on the top of the heap.
      */
@@ -68,39 +68,69 @@ public class _215_Kth_Largest_Element {
      */
     public int findKthLargest_2(int[] nums, int k) {
 
-        /*
-        create the heap with Kth element
-        create heap of Kth element (sort them with lowest at top of the heap and highest at the bottom of the heap)
-         */
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int left = 0, right = nums.length - 1;
 
-        for (int counter = 0; counter < k; counter++) {
-            minHeap.offer(nums[counter]);
+        int k_Th_indexValue = nums.length - k;
+        while (true) {
+
+            int pivotIndex = right;
+            int partitionIndex = partition(nums, left, right, pivotIndex);
+
+            // kth the largest meaning kth element from end of the array
+            if (partitionIndex == k_Th_indexValue) {
+                return nums[partitionIndex];
+            } else if (partitionIndex > k_Th_indexValue) {
+                right = partitionIndex - 1;
+            } else {
+                left = partitionIndex + 1;
+            }
         }
+    }
 
-        int temp = 0;
-        for (int counter = k; counter < nums.length; counter++) {
-            temp = nums[counter];
-            if (temp > minHeap.peek()) {
-                minHeap.poll();
-                minHeap.offer(temp);
+    public int partition(int[] nums, int left, int right, int pivotIndex) {
+
+        int pivotValue = nums[pivotIndex];
+        int higherValueIndex = left;
+
+        for (int counter = left; counter < right; counter++) {
+            if (nums[counter] < pivotValue) {
+                swap(nums, higherValueIndex, counter);
+                higherValueIndex++;
             }
         }
 
-        return minHeap.peek();
+        swap(nums, higherValueIndex, pivotIndex);
+
+        return higherValueIndex;
+    }
+
+    private void swap(int[] intArray, int index1, int index2) {
+        int temp = intArray[index1];
+        intArray[index1] = intArray[index2];
+        intArray[index2] = temp;
     }
 
     @Test
-    public void test1() {
+    public void test1_1() {
         _215_Kth_Largest_Element t = new _215_Kth_Largest_Element();
         assertEquals(5, t.findKthLargest_1(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    }
+
+    @Test
+    public void test1_2() {
+        _215_Kth_Largest_Element t = new _215_Kth_Largest_Element();
+        assertEquals(4, t.findKthLargest_1(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+    }
+
+    @Test
+    public void test2_1() {
+        _215_Kth_Largest_Element t = new _215_Kth_Largest_Element();
         assertEquals(5, t.findKthLargest_2(new int[]{3, 2, 1, 5, 6, 4}, 2));
     }
 
     @Test
-    public void test2() {
+    public void test2_2() {
         _215_Kth_Largest_Element t = new _215_Kth_Largest_Element();
-        assertEquals(4, t.findKthLargest_1(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
         assertEquals(4, t.findKthLargest_2(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
     }
 
