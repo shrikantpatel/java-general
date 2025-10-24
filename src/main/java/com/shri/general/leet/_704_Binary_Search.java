28a788a22a0046b8f177fcf588500dfa3814579e
Shrikant Patel <shrikant_patelin@yahoo.com>
2025-10-23 19:50:10 -0500

    Solved leet code problem 704.

---
+++ src/main/java/com/shri/general/leet/_704_Binary_Search.java
@@ -0,0 +1,77 @@
+package com.shri.general.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _704_Binary_Search {
+
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+
+        while (low <= high) {
+
+            mid = (high - low) / 2 + low;
+            if (target == nums[mid])
+                return mid;
+            if (target > nums[mid])
+                low = mid + 1;
+            if (target < nums[mid])
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+    
+    @Test
+    void testFoundAtBeginning() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {1, 2, 3, 4};
+        assertEquals(0, bs.search(nums, 1));
+    }
+
+    @Test
+    void testFoundAtMiddle() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(2, bs.search(nums, 3));
+    }
+
+    @Test
+    void testFoundAtEnd() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {2, 4, 6, 8};
+        assertEquals(3, bs.search(nums, 8));
+    }
+
+    @Test
+    void testNotFound() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {1, 3, 5, 7};
+        assertEquals(-1, bs.search(nums, 4));
+    }
+
+    @Test
+    void testEmptyArray() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {};
+        assertEquals(-1, bs.search(nums, 10));
+    }
+
+    @Test
+    void testSingleElementFound() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {42};
+        assertEquals(0, bs.search(nums, 42));
+    }
+
+    @Test
+    void testSingleElementNotFound() {
+        _704_Binary_Search bs = new _704_Binary_Search();
+        int[] nums = {42};
+        assertEquals(-1, bs.search(nums, 7));
+    }
+}
