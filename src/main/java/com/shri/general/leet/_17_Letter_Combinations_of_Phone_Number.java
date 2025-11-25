073196b83bf08a4a84c4a8e6987a137f6625ce4e
Shrikant Patel <shrikant_patelin@yahoo.com>
2025-11-24 23:11:06 -0600

    modified solution 17.

--- src/main/java/com/shri/general/leet/_17_Letter_Combinations_of_Phone_Number.java
+++ src/main/java/com/shri/general/leet/_17_Letter_Combinations_of_Phone_Number.java
@@ -7,56 +7,50 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ */
 public class _17_Letter_Combinations_of_Phone_Number {
 
     public List<String> letterCombinations(String digits) {
 
         List<String> ans = new ArrayList<>();
-        String previousString = "";
         int currentIndex = 0;
 
-        if (digits.length() > 0) buildPermutation(previousString, currentIndex, ans, digits);
+        if (!digits.isEmpty()) buildPermutation("", currentIndex, ans, digits);
 
         return ans;
     }
 
     private void buildPermutation(String previousString, int currentIndex, List<String> ans, String digits) {
 
-        char[] charArray = getCharsForDigit(digits.charAt(currentIndex));
+        // add the string to answer and retun if all the digits are accounted for.
+        if (currentIndex >= digits.length()) {
+            ans.add(previousString);
+            return;
+        }
 
-        for (char c : charArray) {
-            // if we are last digit, we add to previousString and add it to answer
-            if (currentIndex == digits.length() - 1) {
-                ans.add(previousString + c);
-            }
-            // else we continue to add it to prviousString and continue to build permutation
-            else {
-                buildPermutation(previousString + c, currentIndex + 1, ans, digits);
-            }
+        // get all the characters
+        char[] allChars = getCharsForDigit(digits.charAt(currentIndex));
+
+        for (char c : allChars) {
+            buildPermutation(previousString + c, currentIndex+1, ans, digits);
         }
     }
 
     private char[] getCharsForDigit(char c) {
 
-        switch (c) {
-            case '2':
-                return new char[]{'a', 'b', 'c'};
-            case '3':
-                return new char[]{'d', 'e', 'f'};
-            case '4':
-                return new char[]{'g', 'h', 'i'};
-            case '5':
-                return new char[]{'j', 'k', 'l'};
-            case '6':
-                return new char[]{'m', 'n', 'o'};
-            case '7':
-                return new char[]{'p', 'q', 'r', 's'};
-            case '8':
-                return new char[]{'t', 'u', 'v'};
-            case '9':
-                return new char[]{'w', 'x', 'y', 'z'};
-        }
-        return new char[0];
+        return switch (c) {
+            case '2' -> new char[]{'a', 'b', 'c'};
+            case '3' -> new char[]{'d', 'e', 'f'};
+            case '4' -> new char[]{'g', 'h', 'i'};
+            case '5' -> new char[]{'j', 'k', 'l'};
+            case '6' -> new char[]{'m', 'n', 'o'};
+            case '7' -> new char[]{'p', 'q', 'r', 's'};
+            case '8' -> new char[]{'t', 'u', 'v'};
+            case '9' -> new char[]{'w', 'x', 'y', 'z'};
+            default -> new char[0];
+        };
     }
 
     @Test
