281112c7db352fbe54c1157033f5795190e4527a
Shrikant Patel <shrikant_patelin@yahoo.com>
2025-11-26 20:24:12 -0600

    Solved leet code 310.

--- src/main/java/com/shri/general/questions/App.java
+++ src/main/java/com/shri/general/questions/App.java
@@ -14,10 +14,6 @@ public class App {
         commList.add(Arrays.asList("TRANSFER", "account1", "account2", "2701"));
         commList.add(Arrays.asList("TRANSFER", "account1", "account2", "200"));
 
-        // CREATE_ACCOUNT,account2
-        // DEPOSIT,non-existing,2700
-        // DEPOSIT,account1,2700
-
         AccountManagement mgmt = new AccountManagement();
 
         for (List<String> command : commList) {
@@ -31,77 +27,91 @@ public class App {
                 case "TRANSFER":
                     System.out.println(mgmt.transfer(command.get(1), command.get(2), Integer.parseInt(command.get(3))));
                     break;
-
             }
         }
-
     }
-
 }
 
+/**
+ * Represents a bank account with an ID and balance.
+ */
+class Account {
+    private final String accountId;
+    private int balance;
 
+    public Account(String accountId) {
+        this.accountId = accountId;
+        this.balance = 0;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(int amount) {
+        if (amount > balance) return false;
+        balance -= amount;
+        return true;
+    }
+}
+
+/**
+ * Manages accounts and operations like create, deposit, withdraw, transfer.
+ */
 class AccountManagement {
-
-    Map<String, Integer> accountDetails = new HashMap<>();
+    private final Map<String, Account> accounts = new HashMap<>();
 
     public boolean createAccount(String accountId) {
-
-
-        if (accountId != null && !accountDetails.containsKey(accountId)) {
-            accountDetails.put(accountId, 0);
+        if (accountId != null && !accounts.containsKey(accountId)) {
+            accounts.put(accountId, new Account(accountId));
             return true;
         }
-
         return false;
     }
 
     public int transaction(String accountId, int amount, String transactionType) {
-
         if (amount < 1) return -1;
 
-        if (accountId != null && accountDetails.containsKey(accountId)) {
+        Account account = accounts.get(accountId);
+        if (account == null) return -1;
 
-            //System.out.println("account - "+ accountId);
-            //System.out.println("amount - "+ amount);
-
-            if (("DEPOSIT").equals(transactionType)) {
-
-                int currentBalance = accountDetails.get(accountId);
-                currentBalance += amount;
-                accountDetails.put(accountId, currentBalance);
-                return currentBalance;
-            } else if (("WITHDRAW").equals(transactionType)) {
-
-                int currentBalance = accountDetails.get(accountId);
-                currentBalance -= amount;
-                accountDetails.put(accountId, currentBalance);
-                return currentBalance;
+        return switch (transactionType) {
+            case "DEPOSIT" -> {
+                account.deposit(amount);
+                yield account.getBalance();
             }
-        }
-
-        return -1;
+            case "WITHDRAW" -> {
+                if (account.withdraw(amount)) {
+                    yield account.getBalance();
+                }
+                yield -1;
+            }
+            default -> -1;
+        };
     }
 
-    // This should transfer the given amount of money from account with `fromId` to account with `toId`.
     public int transfer(String fromId, String toId, int amount) {
-
-        // - Returns -1 if `fromId` and `toId` are the same.
         if (fromId == null || toId == null || fromId.equals(toId)) return -1;
 
-        // - Returns -1 if `fromId` or `toId` doesn't exist.
-        if (!accountDetails.containsKey(fromId)) return -1;
-        if (!accountDetails.containsKey(toId)) return -1;
+        Account fromAccount = accounts.get(fromId);
+        Account toAccount = accounts.get(toId);
 
-        // - Returns -1 if funds on the account `fromId` are insufficient to perform the transfer.
-        int fromBalance = accountDetails.get(fromId);
+        if (fromAccount == null || toAccount == null) return -1;
+        if (fromAccount.getBalance() < amount) return -1;
 
-        if (fromBalance < amount) return -1;
-
-        // - Returns the balance of `fromId` if the transfer was successful, or -1 otherwise.
-        transaction(toId, amount, "DEPOSIT");
-        return transaction(fromId, amount, "WITHDRAW");
-
-
+        // Perform transfer
+        if (fromAccount.withdraw(amount)) {
+            toAccount.deposit(amount);
+            return fromAccount.getBalance();
+        }
+        return -1;
     }
 }
-
